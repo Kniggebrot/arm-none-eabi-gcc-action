f3d1b6f6278201ea3d1abab32e444009ee9227e7
Florian Knigge <flknigge@gmail.com>
2026-06-10 18:23:37 +0200

    chore: add missing types for new functions
    Adressing linter issues.
    Now only returning strings, either path to installed `arm-none-eabi-gcc`,
    or empty string if not found.

--- src/setup.ts
+++ src/setup.ts
@@ -137,7 +137,7 @@ export function findGcc(root: string, platform?: string): string {
   return findGccRecursive(root, `arm-none-eabi-gcc${platform === 'win32' ? '.exe' : ''}`);
 }
 
-async function saveToCache(extractedPath: string, distHash: string, cacheKey: string) {
+async function saveToCache(extractedPath: string, distHash: string, cacheKey: string): Promise<void> {
   core.info(`Adding to cache: ${extractedPath}`);
   await fs.promises.writeFile(path.join(extractedPath, 'md5.txt'), distHash, {
     encoding: 'utf8',
@@ -150,7 +150,7 @@ async function saveToCache(extractedPath: string, distHash: string, cacheKey: st
 }
 
 // returns path to gcc installation downloaded from cache, or undefined if it wasn't found or was wrong.
-async function loadFromCache(installPath: string, cacheKey: string, distData: GccDownloadInfo) {
+async function loadFromCache(installPath: string, cacheKey: string, distData: GccDownloadInfo): Promise<string> {
   // Try to load the GCC installation from the cache
   let cacheKeyMatched: string | undefined = undefined;
   try {
@@ -158,7 +158,7 @@ async function loadFromCache(installPath: string, cacheKey: string, distData: Gc
     core.debug(`Matched cache.restoreCache() key: ${cacheKeyMatched}`);
   } catch (err) {
     core.warning(`⚠️ Could not find contents in the cache.\n${err.message}`);
-    return;
+    return '';
   }
   if (cacheKeyMatched === cacheKey) {
     core.info(`Cache found: ${installPath}`);
@@ -169,17 +169,18 @@ async function loadFromCache(installPath: string, cacheKey: string, distData: Gc
       });
     } catch (err) {
       core.warning(`⚠️ Could not read the contents of the cached GCC version MD5.\n${err.message}`);
-      return;
+      return '';
     }
     core.info(`Cached version MD5: ${cacheMd5}`);
     if (cacheMd5 !== distData.md5) {
       core.warning(`⚠️ Cached version MD5 does not match: ${cacheMd5} != ${distData.md5}`);
-      return;
+      return '';
     } else {
       core.info('Cached version loaded.');
       return installPath;
     }
   }
+  return '';
 }
 
 async function loadFromToolsCache(
@@ -189,7 +190,7 @@ async function loadFromToolsCache(
   arch: string,
   cacheKey: string,
   useCache: boolean
-) {
+): Promise<string> {
   // hosted tools cache should always have the tools matching its platform...
   const hcPath = tc.find(toolName, toolVersion, arch);
   const hcMd5 = await fs.promises.readFile(path.join(hcPath, 'md5.txt'), 'utf8').catch(e => {
@@ -209,4 +210,5 @@ async function loadFromToolsCache(
       return hcPath;
     }
   }
+  return '';
 }
